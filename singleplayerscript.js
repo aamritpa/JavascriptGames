0043fad505c13b72afc5db04fc2da4d5cb26098d
Amritpal Singh <aamritpa@sfu.ca>
2020-09-05 17:17:22 -0700

    Object start moving

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -1,13 +1,32 @@
+
+/*
 class Point {
     constructor(length){
         this.size=length;
     }
 }
+*/
+var circle;
+var paper;
 
-
+function mouseposition(event)
+{
+        var valueX= event.pageX;
+        var valueY= event.pageY;
+        shape_attr = {
+            'transform': 't' + valueX  + ',' + valueY,
+            }
+        circle.animate(shape_attr,2000);  
+}
 
 setup = function() {
-    paper = Raphael('container',2000,1000)
-    $('button').click(make)
+    paper =Raphael("container",2000,2000)
+    circle = paper.circle(250, 150, 30).attr({ 
+        fill: "#ACD", 
+        stroke: "#000", 
+        "stroke-width": 2
+    });
+    $(document).mousemove(mouseposition)
   }
-  $(document).ready(setup)
+
+$(document).ready(setup)
