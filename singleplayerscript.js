d351bf0ac2f3afb6e4a1c996501aff788c9797b3
Amritpal Singh <aamritpa@sfu.ca>
2020-09-07 16:45:11 -0700

    motion failure

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -17,36 +17,61 @@ var firstX=0;
 var firstY=0;
 var valueX=0;
 var valueY=0;
-function objectmovement(event)
+function mousemovement(event)
 {
-    if(firstposition==true)
+    
+    firstX=valueX;
+    firstY=valueY;
+        
+    valueX= event.clientX-250;
+    valueY= event.clientY-150;
+        
+    if((valueY-firstY!=0) ||(valueX-firstX!=0) )
     {
-        firstX=event.clientX-250;
-        firstY=event.clientY-150;
-        firstposition=false;
+        objectmovement(1); // Mouse Move
+    }
+    else
+    {
+        objectmovement(0);  // Mouse does not move
+    }
+}
+function objectmovement(status){
+    if(status==0)
+    {
+        shape_attr = {
+            'transform': 't' + (valueX+100)  + ',' + (valueY+100),
+            }
+        circle.animate(shape_attr,2000);
+        $(document).mousemove(mousemovement); 
+        console.log(valueX);
     }
     else{
-        valueX= event.clientX-250;
-        valueY= event.clientY-150;
-        firstposition=true;
-    }
-    shape_attr = {
-            'transform': 't' + valueX  + ',' + valueY,
+        shape_attr = {
+            'transform': 't' + (valueX)  + ',' + (valueY),
             }
-        circle.animate(shape_attr);  
-        var coords = "X coords: " + valueX + ", Y coords: " + valueY;
-        document.getElementById("demo").innerHTML = coords;
-       
-        if(valueX-firstX>0)
-        {
-            window.scrollBy(2, 0);
+        circle.animate(shape_attr,2000);
+    }
+    /*
+    var objectmovementX=valueX;
+    var objectmovementY=valueY;
+    shape_attr = {
+        'transform': 't' + (valueX+20)  + ',' + (valueY+20),
         }
-        
-        if(valueY-firstY>0 )
-        {
-            window.scrollBy(0, 2);
-        }
-          
+    circle.animate(shape_attr);  
+    /*
+    var coords = "X coords: " + valueX + ", Y coords: " + valueY;
+    document.getElementById("demo").innerHTML = coords;
+  
+    if(valueX-firstX>0)
+    {
+        window.scrollBy(2, 0);
+    }
+    
+    if(valueY-firstY>0 )
+    {
+        window.scrollBy(0, 2);
+    }
+     */
 }
         
 setup = function() {
@@ -56,7 +81,8 @@ setup = function() {
         stroke: "#000", 
         "stroke-width": 2
     });
-    $(document).mousemove(objectmovement)
+    $(document).mousemove(mousemovement)
+
     $(document).mousemove(pointgenerate)
   }
 
