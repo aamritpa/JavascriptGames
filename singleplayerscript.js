69b97a5b20b2cf03e2b90adcac1c95858deb4e1b
Amritpal Singh <aamritpa@sfu.ca>
2020-09-09 01:19:28 -0700

    Motion updated

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -1,106 +1,70 @@
 var circle; // Single player Circle
 var paper;  // Paper Raphael
-var point_array= [];  // Array to store all points.
+let point_array=[];  // Array to store all points.
 var count=0;    // Count total number of points.
 var firstposition =true;
-
+var startpointX;
+var startpointY;
 function pointgenerate(event)
 {
-    point_array[count] = paper.circle(Math.random()*2500, Math.random()*2000, 5).attr({ 
-        fill: '#f'+Math.floor(Math.random() * 500),
+    if(count<50)
+    {
+    point_array.push(paper.circle(Math.random()*2500, Math.random()*2000, 10).attr({ 
+        fill: '#f'+Math.floor(Math.random() * 100),
         stroke: "#000", 
         "stroke-width": 1
-    });
+    }));
     count=count+1;
+    }
+    $(document).mousemove(objectmovement)
 }
-var firstX=0;
-var firstY=0;
-var valueX=0;
-var valueY=0;
-function mousemovement(event)
-{
-    
-    firstX=valueX;
-    firstY=valueY;
-        
-    valueX= event.clientX-250;
-    valueY= event.clientY-150;
-        
-    if((valueY-firstY!=0) ||(valueX-firstX!=0) )
+
+function objectmovement(event){
+
+    var valueX=event.clientX-250;
+    var valueY=event.clientY-150;
+    var i=0;
+    if(count==50)
     {
-        objectmovement(1); // Mouse Move
-    }
-    else
-    {
-        objectmovement(0);  // Mouse does not move
-    }
-}
-function objectmovement(){
-    /*
-    if(status==0)
-    {
-        shape_attr = {
-            'transform': 't' + (valueX+100)  + ',' + (valueY+100),
+        for(i=0;i<50;i++)
+        {
+            
+            var distance = Math.sqrt(Math.pow((valueX-point_array[i].attr('cx')),2)+Math.pow((valueY-point_array[i].attr('cy')),2));
+            console.log(distance);
+            if(distance<=40)
+            {
+                console.log(point_array[i].attr('cx'));
+                console.log("hello");
+                point_array[i].hide();
+                circle.attr('r')=circle.attr('r')+20;
+                break;
             }
-        circle.animate(shape_attr,2000);
-        $(document).mousemove(mousemovement); 
-        console.log(valueX);
-    }
-    else{
-        shape_attr = {
-            'transform': 't' + (valueX)  + ',' + (valueY),
-            }
-        circle.animate(shape_attr,2000);
-    }
-    /*
-    var objectmovementX=valueX;
-    var objectmovementY=valueY;
-    shape_attr = {
-        'transform': 't' + (valueX+20)  + ',' + (valueY+20),
         }
-    circle.animate(shape_attr);  
-    /*
-    var coords = "X coords: " + valueX + ", Y coords: " + valueY;
-    document.getElementById("demo").innerHTML = coords;
-  
-    if(valueX-firstX>0)
-    {
-        window.scrollBy(2, 0);
+    }
+   console.log("Value of count"+count);
+
+    
+    
+callback =function(){
+
     }
     
-    if(valueY-firstY>0 )
-    {
-        window.scrollBy(0, 2);
-    }
-     */
-    valueY=valueY+1;
-    var anim = Raphael.animation( 
-        { 0.5: 
-        {   transform: "t500,500"}, 1: 
-        { transform: 't-500,-500' } 
-        }, 5000 ).repeat(Infinity);
-    if(valueY%2==0)
-    {
-        circle.stop();
-        
-    }
-    else{
-        circle.animate(anim);
-    }
+    shape_attr = {
+        'transform': 't' + valueX  + ',' + valueY,
+        }
+    circle.animate(shape_attr,'linear',callback);  
 }
-        
+var radius=30;   
 setup = function() {
     paper =Raphael("container",5000,5000)
     
-    circle = paper.circle(250, 150, 30).attr({ 
+    circle = paper.circle(250, 150, radius).attr({ 
         fill: "#ACD", 
         stroke: "#000", 
         "stroke-width": 2
     });
+    $(document).mousemove(pointgenerate)
     
-    //$(document).mousemove(objectmovement)
-
-    //$(document).mousemove(pointgenerate)
-  }
+}
 
 $(document).ready(setup)
