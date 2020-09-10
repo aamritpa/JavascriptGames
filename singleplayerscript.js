627f346951b98ba4431e8c6461020569c2796e03
Amritpal Singh <aamritpa@sfu.ca>
2020-09-10 00:39:56 -0700

    Motion updated. Cirlce eats points now

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -1,22 +1,36 @@
 var circle; // Single player Circle
 var paper;  // Paper Raphael
 let point_array=[];  // Array to store all points.
+
+let pointMap= new Map();
 var count=0;    // Count total number of points.
 var firstposition =true;
 var startpointX;
 var startpointY;
+
 function pointgenerate(event)
 {
-    if(count<50)
-    {
-    point_array.push(paper.circle(Math.random()*2500, Math.random()*2000, 10).attr({ 
+    if(count<500){
+        var x=Math.floor(Math.random()*5000);
+        var y= Math.floor(Math.random()*5000);
+        pointMap.set(x,y);
+        
+        var c= paper.circle(x,y, 10).attr({ 
         fill: '#f'+Math.floor(Math.random() * 100),
-        stroke: "#000", 
+        stroke: "#RGB", 
         "stroke-width": 1
-    }));
+    });
+    c.node.onmouseover= function(){
+        
+        count=count-1;
+        c.remove();
+        radius=radius+20;
+        circle.hide();
+        circle=paper.circle(250,150,radius);
+        
+    }
     count=count+1;
     }
-    $(document).mousemove(objectmovement)
 }
 
 function objectmovement(event){
@@ -24,47 +38,24 @@ function objectmovement(event){
     var valueX=event.clientX-250;
     var valueY=event.clientY-150;
     var i=0;
-    if(count==50)
-    {
-        for(i=0;i<50;i++)
-        {
-            
-            var distance = Math.sqrt(Math.pow((valueX-point_array[i].attr('cx')),2)+Math.pow((valueY-point_array[i].attr('cy')),2));
-            console.log(distance);
-            if(distance<=40)
-            {
-                console.log(point_array[i].attr('cx'));
-                console.log("hello");
-                point_array[i].hide();
-                circle.attr('r')=circle.attr('r')+20;
-                break;
-            }
-        }
-    }
-   console.log("Value of count"+count);
 
-    
-    
-callback =function(){
 
-    }
-    
     shape_attr = {
         'transform': 't' + valueX  + ',' + valueY,
         }
-    circle.animate(shape_attr,'linear',callback);  
+    circle.animate(shape_attr);
+    
 }
-var radius=30;   
+var radius=50;   
 setup = function() {
     paper =Raphael("container",5000,5000)
-    
     circle = paper.circle(250, 150, radius).attr({ 
         fill: "#ACD", 
         stroke: "#000", 
         "stroke-width": 2
     });
     $(document).mousemove(pointgenerate)
-    
+    $(document).mousemove(objectmovement)
 }
 
 $(document).ready(setup)
