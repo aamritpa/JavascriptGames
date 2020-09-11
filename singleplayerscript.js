1e852d1f196c449e82608dfe7ab7f126eb7c6d78
Amritpal Singh <aamritpa@sfu.ca>
2020-09-11 00:31:11 -0700

    Scroll bar updated

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -1,3 +1,4 @@
+
 var circle; // Single player Circle
 var paper;  // Paper Raphael
 let point_array=[];  // Array to store all points.
@@ -15,30 +16,32 @@ function pointgenerate(event)
         var y= Math.floor(Math.random()*5000);
         pointMap.set(x,y);
         
-        var c= paper.circle(x,y, 10).attr({ 
+        var c= paper.circle(x,y, 20).attr({ 
         fill: '#f'+Math.floor(Math.random() * 100),
         stroke: "#RGB", 
         "stroke-width": 1
     });
+    
     c.node.onmouseover= function(){
         
         count=count-1;
         c.remove();
-        radius=radius+20;
-        circle.hide();
+        radius=radius+2;
+        /*
+        circle.remove();
         circle=paper.circle(250,150,radius);
-        
+        */
     }
     count=count+1;
     }
 }
+var move =false;
+var down=false;
 
 function objectmovement(event){
-
+    move =true;
     var valueX=event.clientX-250;
     var valueY=event.clientY-150;
-    var i=0;
-
 
     shape_attr = {
         'transform': 't' + valueX  + ',' + valueY,
@@ -46,6 +49,16 @@ function objectmovement(event){
     circle.animate(shape_attr);
     
 }
+/* This need to be updated. This whole function shoulg be called from 'setup'
+$(document).mousemove(function(e) {
+    $("html, body").scrollTop(function(i, v) {
+       var h = $(window).height();
+       var y = e.clientY - h / 2;   //determins the y location where your scrolling starts
+       return v + y * 0.05;          //0.1 determines the speed
+     });
+  });
+*/
+
 var radius=50;   
 setup = function() {
     paper =Raphael("container",5000,5000)
