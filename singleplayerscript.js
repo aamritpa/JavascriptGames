8339450d16f8ea9b9a534526de6591a21bc8c9c3
Amritpal Singh <aamritpa@sfu.ca>
2020-11-19 19:03:36 -0800

    UI updated for Happy Hunt Game

--- singleplayerscript.js
+++ singleplayerscript.js
@@ -1,23 +1,52 @@
-
+//Global Variables 
 var circle; // Single player Circle
 var paper;  // Paper Raphael
-let point_array=[];  // Array to store all points.
-
-let pointMap= new Map();
 var count=0;    // Count total number of points.
 var firstposition =true;
 var startpointX;
 var startpointY;
 var scrollermove = false;
+var score=0;
 
+var countDownDate = 30;
+
+//Reloading the Page
+function restart()
+{
+    location.reload(true);
+}
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+    countDownDate=countDownDate-1;
+    document.getElementById("Time").innerHTML = "Time Left: "+countDownDate + "s ";
+    
+  // If the count down is over, write some text 
+  if (countDownDate < 0) {
+    clearInterval(x);
+    document.getElementById("Time").innerHTML = "Time UP!";
+
+    if(score>=50){
+        alert("Winner! Your Score is: "+score);
+        score=0;
+        countDownDate=30;  
+    }
+    else{
+        alert("Time Up! You Lose!");
+        score=0;
+        countDownDate=30;
+        
+    }
+  }
+}, 1000);
+
+//Generating Randomly Points
 function pointgenerate(event)
 {
     if(count<500){
-        var x=Math.floor(Math.random()*5000);
-        var y= Math.floor(Math.random()*5000);
-        pointMap.set(x,y);
-        
-        var c= paper.circle(x,y, 20).attr({ 
+        var x=Math.floor(Math.random()*2000);
+        var y= Math.floor(Math.random()*2000);
+        var c= paper.circle(x,y, 5).attr({ 
         fill: '#f'+Math.floor(Math.random() * 100),
         stroke: "#RGB", 
         "stroke-width": 1
@@ -28,17 +57,15 @@ function pointgenerate(event)
         count=count-1;
         c.remove();
         radius=radius+2;
-        /*
-        circle.remove();
-        circle=paper.circle(250,150,radius);
-        */
+        score=score+1;
+        document.getElementById("score").innerHTML = "Score: "+score;
     }
     count=count+1;
     }
 }
 var move =false;
 var down=false;
-
+//Movement of circle
 function objectmovement(event){
     move =true;
     var valueX=event.clientX-250;
@@ -47,24 +74,15 @@ function objectmovement(event){
     shape_attr = {
         'transform': 't' + valueX  + ',' + valueY,
         }
-    circle.animate(shape_attr,2);
+    circle.animate(shape_attr,0);
     
 }
-/* This need to be updated. This whole function shoulg be called from 'setup' */
-$(document).mousemove(function(e) {
-    $("html, body").scrollTop(function(i, v) {
-       var h = $(window).height();
-       var y = e.clientY - h / 2;   //determins the y location where your scrolling starts
-       return v + y * 0.05;          //0.1 determines the speed
-       objectmovement(e);
-     });
-  });
 
-
-var radius=50;   
+//JQUery Function calling and setting game window size
+var radius=20;   
 setup = function() {
-    paper =Raphael("container",5000,5000)
-    circle = paper.circle(250, 150, radius).attr({ 
+    paper =Raphael("container",2000,1000)
+    circle = paper.circle(250, -20, radius).attr({ 
         fill: "#ACD", 
         stroke: "#000", 
         "stroke-width": 2
